26abafd2ec90a929b4303c905688d28e2f27ce33
Diego Carrera <dcarreraacosta@gmail.com>
2023-11-12 11:37:48 -0600

    new desing

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,6 @@
 import MainPresentation from './custom/home'
 import './style.css'
+import './css/home.css'
+import './css/nav.css'
 
 
-const app = document.querySelector('#app');
-const elements = {
-    creations: document.querySelector('#creations'),
-}
-
-
-elements.creations?.addEventListener('click',():void=>{
-    console.log('clickeds')
-        if (app?.hasChildNodes) app.innerHTML = ''
-        
-        const custom:HTMLElement = new MainPresentation;
-        app?.appendChild(custom);
-        const img = document.querySelectorAll('#app img');
-        img[0].addEventListener('load',(): void =>{
-            console.log('image load')
-        })
-    
-})
--- src/template/home.ts
+++ src/template/home.ts
@@ -1,25 +1,6 @@
 const template = 
 /*html*/`
-<div id="main" class="animate__animated animate__fadeIn" >
-    <img class="float" src="/images/stickers/1.png" alt="" />
-    <section>
-      <h1>¿Necesitas crear una tarea escolar?</h1>
-      <p>podemos crear manualidades escolares, descubre más</p>
-    </section>
-    <section>
-      <div id="button-stars">
-        <div id="stars-container">
-          <img class="star" src="/images/icons/star.png" alt="">
-          <img class="star" src="/images/icons/star.png" alt="">
-          <img class="star" src="/images/icons/star.png" alt="">
-          <img class="star" src="/images/icons/star.png" alt="">
-          <div>sada</div>
-        </div>
-        <button>DESCUBRIR</button>
-      </div>
-    </section>
-    </main>
-   </div>
+
 `
 
 export default template;
